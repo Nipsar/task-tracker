92f290958bcc353611e273ac66b5709247510771
Artem Mihalev <mihalevartem0606@gmail.com>
2026-01-03 03:13:16 +0500

    first commit

---
+++ src/main/java/com/example/tasktacker/TaskTackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.tasktacker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tasktacker/TaskTackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tasktacker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
